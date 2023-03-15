3a2c4707b07326cf3ad5515ddd7384b008170403
Erenilda Tavares <erenyldajsilva@gmail.com>
2023-03-14 23:16:33 -0300

    Exercicos generation

---
+++ generation/src/exercicio1Gen.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class exercicio1Gen {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        float salario = 10000.00f;
+        float novoSalario = 11000.00f;
+        float abono = 1000.00f;
+
+        System.out.println("\n Digite o salário: ");
+        salario = scan.nextFloat();
+        System.out.println("\n Digite o Abono: ");
+        abono = scan.nextFloat();
+
+        novoSalario = salario + abono;
+
+        System.out.println("\n Novo salário é: "+ novoSalario);
+
+        scan.close();
+    }
+}
