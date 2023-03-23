e4a0356bfce82f1e46e00435545fc5307bc96761
Erenilda Tavares <erenyldajsilva@gmail.com>
2023-03-23 17:13:09 -0300

    alteracoes

---
+++ generation/src/exerciciosGeneration/exerciciosDia14/ex4Generation.java
@@ -0,0 +1,25 @@
+package exerciciosDia14;
+import java.util.Scanner;
+
+public class ex4Generation {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        float n1, n2, n3, n4, diferenca;
+
+        System.out.println("digite o valor do numero 1: ");
+        n1 = scan.nextFloat();
+        System.out.println("digite o valor do numero 2: ");
+        n2 = scan.nextFloat();
+        System.out.println("digite o valor do numero 3: ");
+        n3 = scan.nextFloat();
+        System.out.println("digite o valor do numero 4: ");
+        n4 = scan.nextFloat();
+
+        diferenca = (n1 * n2) - (n3 * n4);
+
+        System.out.printf("A diferença é:%.2f ", diferenca);
+
+        scan.close();
+    }
+}
