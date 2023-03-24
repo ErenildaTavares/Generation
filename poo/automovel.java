244658b2a24f59611d261fcfd35e94d78ea614a7
Erenilda Tavares <erenyldajsilva@gmail.com>
2023-03-24 18:11:27 -0300

    organizando pastas

---
+++ poo/automovel.java
@@ -0,0 +1,46 @@
+package poo;
+
+import java.lang.reflect.Constructor;
+
+import javax.xml.transform.Source;
+
+public class automovel {
+    //declaracao dos atributos da classe.
+    //private so a classe automovel pode acessar
+
+    private String nomeProprietario;
+    private String modelo;
+    private String placa;
+    private int ano;
+    public String getNomeProprietario() {
+        return nomeProprietario;
+    }
+    public void setNomeProprietario(String nomeProprietario) {
+        this.nomeProprietario = nomeProprietario;
+    }
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public String getPlaca() {
+        return placa;
+    }
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+    public int getAno() {
+        return ano;
+    }
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    //metodo construtor.
+    
+
+
+
+
+}
---
+++ poo/empregado.java
@@ -0,0 +1,38 @@
+package poo;
+
+import java.text.NumberFormat;
+
+public class empregado {
+    private String nome;
+    private double salario;
+    public empregado(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;   
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public double getSalario() {
+        return salario;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    public void aumentarSalario(double percentual){
+        salario *= 1 + percentual /100;
+    }
+
+    public String formatarMoeda(){
+
+        NumberFormat nf = NumberFormat.getCurrencyInstance();
+        nf.setMaximumFractionDigits(2);
+        String formatoMoeda = nf.format(salario);
+        return formatoMoeda;
+    }
+   public void imprimir(){
+    System.out.println(nome+ " \t\t\tSalário: "+this.formatarMoeda());
+   }
+}
